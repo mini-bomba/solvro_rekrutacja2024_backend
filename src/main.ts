f326e2f21b0df7ec494b52830e4b8fce12323546
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2024-10-08 18:48:22 +0200

    Set up API docs generator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Coctails API')
+    .setDescription('A simple coctail recipe management REST API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
